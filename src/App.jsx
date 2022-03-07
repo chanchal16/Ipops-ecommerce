1d6f977204b0eb23449d50eac650d50f82726820
chanchal16 <rajputchanchal17@gmail.com>
2022-03-07 22:42:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
+import Sidebar from './components/sidebar'
+import Products from './views/products'
 
 function App() {
 
   return (
     <div className="App">
       <Navbar />
-      <main></main>
+      <main>
+        {/* <Sidebar/> */}
+        <Products/>
+      </main>
       <Footer/>
     </div>
   )
