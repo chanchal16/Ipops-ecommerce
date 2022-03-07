75ebf7658a213b5ad118fdbdcabaf597d9ab54f9
chanchal16 <rajputchanchal17@gmail.com>
2022-03-07 21:54:37 +0530

    Create - mock data

---
+++ src/store/products.js
@@ -0,0 +1,84 @@
+const products = [
+    {
+        id:1,
+        name:'Vincent Chase',
+        description:'Blue Transparent Full Rim Round Eyeglasses',
+        type:'eg',
+        price:799,
+        img:'https://cdn.eyemyeye.com/shared/images/products/E20C4254/E20C4254-1-hd.jpg',
+        rating:4.0
+    },
+    {
+        id:2,
+        name:'Vistazo',
+        description:'Black Full Rim Round Eyeglasses',
+        type:'eg',
+        price:999,
+        img:'https://cdn.eyemyeye.com/shared/images/products/E20A3743/E20A3743-1-hd.jpg',
+        rating:3.7
+    },
+    {
+        id:3,
+        name:'Vistazo',
+        description:'Blue Tinted Aviator Sunglasses',
+        type:'sg',
+        price:1299,
+        img:'https://cdn.eyemyeye.com/shared/images/products/S20A2407/S20A2407-1-hd.jpg',
+        rating:4.2
+    },
+    {
+        id:4,
+        name:'Nerdlane',
+        description:'Gunmetal Full Rim Aviator Computer glasses',
+        type:'cg',
+        price:899,
+        img:'https://cdn.eyemyeye.com/shared/images/products/E20A4096/E20A4096-1-hd.jpg',
+        rating:4.0
+    },
+    {
+        id:5,
+        name:'Vincent Chase',
+        description:'Smoke Tinted Cateye Sunglasses',
+        type:'sg',
+        price:1450,
+        img:'https://cdn.eyemyeye.com/shared/images/products/S66A1714/S66A1714-1-hd.jpg',
+        rating:3.8
+    },
+    {
+        id:6,
+        name:'Nerdlane',
+        description:'Black metal Full Rim Aviator Computer glasses',
+        type:'cg',
+        price:2600,
+        img:'https://cdn.eyemyeye.com/shared/images/products/E12A3261/E12A3261-1-hd.jpg',
+        rating:4.3
+    },
+    {
+        id:7,
+        name:'Vistazo',
+        description:'Glossy Multicolor Full Rim Cat Eye Eyeglasses',
+        type:'eg',
+        price:1200,
+        img:'https://cdn.eyemyeye.com/shared/images/products/E18A3477/E18A3477-1-hd.jpg',
+        rating:3.9
+    },
+    {
+        id:8,
+        name:'Vincent Chase',
+        description:'Silver Full Frame Round Computer Glasses ',
+        type:'cg',
+        price:1499,
+        img:'https://cdn.eyemyeye.com/shared/images/products/E10B3446/E10B3446-1-hd.jpg',
+        rating:4.1
+    },
+    {
+        id:9,
+        name:'Vincent Chase',
+        description:'Multicolor Mirror Clubmaster Sunglasses',
+        type:'sg',
+        price:1800,
+        img:'https://cdn.eyemyeye.com/shared/images/products/S66B2142/S66B2142-1-hd.jpg',
+        rating:4.2
+    }
+]
+export {products};
