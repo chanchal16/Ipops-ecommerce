399a6e5bb05c3b8de4f987ceb2afa75937d0b31d
chanchal16 <rajputchanchal17@gmail.com>
2022-03-11 22:59:56 +0530

    Update - add filters

--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
 import Sidebar from '../components/sidebar';
+import { useFilter } from '../contexts/filterContext';
 import { products } from '../store/products';
 
 export default function Products() {
+    const {state,dispatch} = useFilter()
+    console.log('state from prod',state)
+
+    const sortPrice = ()=>{
+        state.price === 'high'?()=> dispatch({ type: "HIGH_TO_LOW" }): ()=>dispatch({type: "LOW_TO HIGH" })
+    }
   return (
     <div className="main-container">
         <Sidebar/>
         <section>
+            <div className="sort-div">
+                {/*<label htmlFor="sort-items">Sort by:</label>
+                <select id="sort-items" name="sort-items" onChange={sortPrice }>
+                    <option value="low" >Low to High</option>
+                    <option value="high" >High to Low</option>
+                </select>*/}
+                <input
+                type="radio"
+                name="radiobtn"
+                onClick={() => dispatch({ type: "LOW_TO HIGH" })}
+                />
+                <label>low to high</label>
+                <input
+                type="radio"
+                name="radiobtn"
+                onClick={() => dispatch({ type: "HIGH_TO_LOW" })}
+                />
+                <label>high to low</label>
+            </div>
+            {/* <!---product list----> */}
             <div className='product-list'>
             {
-            products.map(product=>(
+            state.map(product=>(
                 <div class="card">
                     <div class="card-media">
                         <img class="vc-image" 
@@ -25,7 +52,7 @@ export default function Products() {
                             </span>
                         </div>
                         <div class="desc">
-                            <p class="desc-title">Eyeglasses</p>
+                            <p class="desc-title">{product.type}</p>
                             <p><strong> {product.price} </strong> <span class="strike-text gray">Rs.999
                                 </span> <span class="secondary">20% off</span></p>
                         </div>
