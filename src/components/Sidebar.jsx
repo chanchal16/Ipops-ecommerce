65afb1c84a3540d50badd9155a7a890f14a1db88
chanchal16 <rajputchanchal17@gmail.com>
2022-03-11 22:58:14 +0530

    Update - add filter functionality

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useFilter } from '../contexts/filterContext'
 
 export default function Sidebar() {
+    const {state,dispatch} = useFilter()
   return (
     <div>
          <aside>
@@ -8,8 +10,9 @@ export default function Sidebar() {
                 <span class="h6 sidebar-heading">Filters</span>
                 <div class="filters">
                     <p class="text-md">Price</p>
-                    <input type="range" min="100" max="1000" value="500" 
-                    class="--slider accent" id="myRange"/>
+                    <input type="range" min="800" max="2000" value={state.price} 
+                    class="--slider accent" id="myRange" 
+                    onChange={(e) => dispatch({ type: "PRICE", payload: e.target.value })} />
                 </div>
                 {/* ---categories-- */}
                 <div class="filters">
@@ -17,11 +20,14 @@ export default function Sidebar() {
                     <label class="text-sm accent" for="checkbox">
                         <input type="checkbox" name="category" /> All </label> 
                     <p class="text-sm accent">
-                        <input type="checkbox" name="category" /> Eye-glasses </p> 
+                        <input type="checkbox" name="category" value='eye-glasses' onClick={(e)=>dispatch({type:'EYEGLASSES'})}/>
+                         Eye-glasses </p> 
                     <p class="text-sm accent">
-                        <input type="checkbox" name="category" /> Computer-glasses </p>
+                        <input type="checkbox" name="category" value='computer-glasses' onClick={(e)=>dispatch({type:'COMPUTERGLASSES'})} />
+                         Computer-glasses </p>
                     <p class="text-sm accent">
-                        <input type="checkbox" name="category" /> Sunglasses </p>
+                        <input type="checkbox" name="category" value='sun-glasses' onClick={(e)=>dispatch({type:'SUNGLASSES'})} />
+                         Sunglasses </p>
                 </div>
                 {/* <!---ratings----> */}
                 <div class="filters">
