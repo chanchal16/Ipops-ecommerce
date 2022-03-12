41f124c8697bfc894ae7f8cae676b66769a8c89f
chanchal16 <rajputchanchal17@gmail.com>
2022-03-12 23:05:40 +0530

    Update - add wishlist context provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { WishlistContextProvider } from './contexts/WishlistContext'
 
 ReactDOM.render(
   <React.StrictMode>
+    <WishlistContextProvider>
     <App />
+    </WishlistContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
