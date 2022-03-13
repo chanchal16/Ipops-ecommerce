bca9234680cc71fca81f015f1f306462158a344d
chanchal16 <rajputchanchal17@gmail.com>
2022-03-13 20:51:31 +0530

    chore - add router

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { BrowserRouter as Router } from "react-router-dom";
 import { WishlistContextProvider } from './contexts/WishlistContext'
 
 ReactDOM.render(
   <React.StrictMode>
-    <WishlistContextProvider>
-    <App />
-    </WishlistContextProvider>
+    <Router>
+      <WishlistContextProvider>
+        <App />
+      </WishlistContextProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
