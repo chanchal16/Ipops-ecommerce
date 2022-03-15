2aff09f56928b946b24c7bd59e83edb89769898c
chanchal16 <rajputchanchal17@gmail.com>
2022-03-15 22:08:48 +0530

    Update - add router

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { BrowserRouter as Router } from "react-router-dom";
 import { CartContextProvider } from './contexts/CartContext'
 
 ReactDOM.render(
   <React.StrictMode>
-    <CartContextProvider>
-      <App />
-    </CartContextProvider>
+    <Router>
+      <CartContextProvider>
+        <App />
+      </CartContextProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
