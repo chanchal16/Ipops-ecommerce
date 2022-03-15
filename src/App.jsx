a54d5549d8894b83eddee75af5c9b1601f345fe4
chanchal16 <rajputchanchal17@gmail.com>
2022-03-15 22:08:06 +0530

    Update - add routes to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
+import { Routes, Route } from "react-router-dom";
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
-import Sidebar from './components/sidebar'
 import Cart from './views/Cart'
 import Products from './views/products'
 
@@ -11,9 +11,11 @@ function App() {
     <div className="App">
       <Navbar />
       <main>
-        {/* <Sidebar/> */}
-        <Products/>
-        <Cart/>
+        <Routes>
+          <Route path="/" element={<Products/>} />
+          <Route path="cart" element={<Cart/>} />
+          
+        </Routes>
       </main>
       <Footer/>
     </div>
