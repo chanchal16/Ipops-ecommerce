bfb448fff1a5c5e497dfb2effdb75b86226466aa
chanchal16 <rajputchanchal17@gmail.com>
2022-03-15 18:21:03 +0530

    chore - add quantity property

--- src/store/products.js
+++ src/store/products.js
@@ -6,7 +6,8 @@ const products = [
         type:'eg',
         price:799,
         img:'https://cdn.eyemyeye.com/shared/images/products/E20C4254/E20C4254-1-hd.jpg',
-        rating:4.0
+        rating:4.0,
+        qty:1
     },
     {
         id:2,
@@ -15,7 +16,8 @@ const products = [
         type:'eg',
         price:999,
         img:'https://cdn.eyemyeye.com/shared/images/products/E20A3743/E20A3743-1-hd.jpg',
-        rating:3.7
+        rating:3.7,
+        qty:1
     },
     {
         id:3,
@@ -24,7 +26,8 @@ const products = [
         type:'sg',
         price:1299,
         img:'https://cdn.eyemyeye.com/shared/images/products/S20A2407/S20A2407-1-hd.jpg',
-        rating:4.2
+        rating:4.2,
+        qty:1
     },
     {
         id:4,
@@ -33,7 +36,8 @@ const products = [
         type:'cg',
         price:899,
         img:'https://cdn.eyemyeye.com/shared/images/products/E20A4096/E20A4096-1-hd.jpg',
-        rating:4.0
+        rating:4.0,
+        qty:1
     },
     {
         id:5,
@@ -42,7 +46,8 @@ const products = [
         type:'sg',
         price:1450,
         img:'https://cdn.eyemyeye.com/shared/images/products/S66A1714/S66A1714-1-hd.jpg',
-        rating:3.8
+        rating:3.8,
+        qty:1
     },
     {
         id:6,
@@ -51,7 +56,8 @@ const products = [
         type:'cg',
         price:2600,
         img:'https://cdn.eyemyeye.com/shared/images/products/E12A3261/E12A3261-1-hd.jpg',
-        rating:4.3
+        rating:4.3,
+        qty:1
     },
     {
         id:7,
@@ -60,7 +66,8 @@ const products = [
         type:'eg',
         price:1200,
         img:'https://cdn.eyemyeye.com/shared/images/products/E18A3477/E18A3477-1-hd.jpg',
-        rating:3.9
+        rating:3.9,
+        qty:1
     },
     {
         id:8,
@@ -69,7 +76,8 @@ const products = [
         type:'cg',
         price:1499,
         img:'https://cdn.eyemyeye.com/shared/images/products/E10B3446/E10B3446-1-hd.jpg',
-        rating:4.1
+        rating:4.1,
+        qty:1
     },
     {
         id:9,
@@ -78,7 +86,8 @@ const products = [
         type:'sg',
         price:1800,
         img:'https://cdn.eyemyeye.com/shared/images/products/S66B2142/S66B2142-1-hd.jpg',
-        rating:4.2
+        rating:4.2,
+        qty:1
     }
 ]
 export {products};
