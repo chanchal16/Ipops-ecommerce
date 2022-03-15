2a97c1cd909ff836e41439000b63bc4fd2454468
chanchal16 <rajputchanchal17@gmail.com>
2022-03-15 18:19:17 +0530

    style - changed class to className

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,43 +4,43 @@ export default function Sidebar() {
   return (
     <div>
          <aside>
-            <div class="sidebar">
-                <span class="h6 sidebar-heading">Filters</span>
-                <div class="filters">
-                    <p class="text-md">Price</p>
+            <div className="sidebar">
+                <span className="h6 sidebar-heading">Filters</span>
+                <div className="filters">
+                    <p className="text-md">Price</p>
                     <input type="range" min="100" max="1000" value="500" 
-                    class="--slider accent" id="myRange"/>
+                    className="--slider accent" id="myRange" onChange={()=>{}}/>
                 </div>
                 {/* ---categories-- */}
-                <div class="filters">
-                    <p class="text-md">Category</p>
-                    <label class="text-sm accent" for="checkbox">
+                <div className="filters">
+                    <p className="text-md">Category</p>
+                    <label className="text-sm accent" htmlFor="checkbox">
                         <input type="checkbox" name="category" /> All </label> 
-                    <p class="text-sm accent">
+                    <p className="text-sm accent">
                         <input type="checkbox" name="category" /> Eye-glasses </p> 
-                    <p class="text-sm accent">
+                    <p className="text-sm accent">
                         <input type="checkbox" name="category" /> Computer-glasses </p>
-                    <p class="text-sm accent">
+                    <p className="text-sm accent">
                         <input type="checkbox" name="category" /> Sunglasses </p>
                 </div>
                 {/* <!---ratings----> */}
-                <div class="filters">
-                    <p class="text-md">Ratings</p>
-                    <p class="text-sm accent" for="radio">
+                <div className="filters">
+                    <p className="text-md">Ratings</p>
+                    <p className="text-sm accent" htmlFor="radio">
                         <input type="radio" name="rating" />
-                         4<i class="fas fa-star fa-xs"></i> & above 
+                         4<i className="fas fa-star fa-xs"></i> & above 
                     </p> 
-                    <p class="text-sm accent" for="radio">
+                    <p className="text-sm accent" htmlFor="radio">
                         <input type="radio" name="rating" />
-                         3<i class="fas fa-star fa-xs"></i> & above
+                         3<i className="fas fa-star fa-xs"></i> & above
                     </p> 
-                    <p class="text-sm accent" for="radio">
+                    <p className="text-sm accent" htmlFor="radio">
                         <input type="radio" name="rating" /> 
-                        2<i class="fas fa-star fa-xs"></i> & above 
+                        2<i className="fas fa-star fa-xs"></i> & above 
                     </p>
-                    <p class="text-sm accent" for="radio">
+                    <p className="text-sm accent" htmlFor="radio">
                         <input type="radio" name="rating" /> 
-                        1<i class="fas fa-star fa-xs"></i> & above 
+                        1<i className="fas fa-star fa-xs"></i> & above 
                     </p>
                 </div>
             </div>
